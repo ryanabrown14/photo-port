1c87482eaf51d57dcaa25de731936744fc54931c
ry brown <ryan.alex.brown@gmail.com>
2020-12-18 19:44:56 -0700

    feature

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,12 +3,22 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
-    it('renders', () => {
-        render(<Nav />);
-    });
+it('renders', () => {
+  render(<Nav
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+  />);
+})
     it('matches snapshot', () => {
         const { asFragment } = render(<Nav />);
         // assert value comparison
